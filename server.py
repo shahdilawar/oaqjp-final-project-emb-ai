539f36cf594254ac6d4e1d3df5a9a53cd25ce691
shahdilawar <shahdilawar@gmail.comm>
2026-04-12 20:19:25 -0400

    added flask server.py

---
+++ server.py
@@ -0,0 +1,44 @@
+'''
+This module serves as Customer facing function using Flask
+'''
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+#Instantiate Flask object
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/emotionDetector")
+def emotion_analyzer():
+
+    # retrieve the input query value
+    text_to_analyze = request.args.get("textToAnalyze")
+
+    # Retrieve the emotion dictionary
+    emotion_dict = emotion_detector(text_to_analyze)
+
+    # Format the output
+    formatted_output = format_output(emotion_dict)
+
+    return formatted_output
+
+
+def format_output(result):
+    '''
+    Utility module to format the output.
+    '''
+    return (
+        f"For the given statement, the system response is "
+        f"'anger': {result['anger']}, "
+        f"'disgust': {result['disgust']}, "
+        f"'fear': {result['fear']}, "
+        f"'joy': {result['joy']} and "
+        f"'sadness': {result['sadness']}. "
+        f"The dominant emotion is {result['dominant_emotion']}."
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
